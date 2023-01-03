06fd71e9bf0f07d108e20b49552fbcec7557f10d
KodingAffanMaulana <affan.maulana2016@students.unila.ac.id>
2023-01-03 22:40:46 +0700

    03/01/2023 kelima

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
 import Experience from './Routes/Experience';
 import Project from './Routes/Project';
