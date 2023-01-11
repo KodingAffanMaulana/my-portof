fc31dce218e0583aec1f85ca559caf37862bc837
KodingAffanMaulana <affan.maulana2016@students.unila.ac.id>
2023-01-11 18:50:25 +0700

    update responsive

--- src/Routes/Education.jsx
+++ src/Routes/Education.jsx
@@ -12,30 +12,30 @@ const Education = () => {
             <div className="drop-shadow-2xl">
                 <section id="education" class="px-8 pb-8 sm:px-16 sm:pb-8 bg-white">
                     <h1 class="font-bold text-[16px] sm:text-2xl text-lime-1000 pt-6 pb-1 sm:pb-3">Riwayat Pendidikan</h1><br />
-                    <div class="sm:flex-col md:flex-col lg:flex-row flex flex-col gap-[10%]">
+                    <div class="sm:flex-col lg:text-[2px] md:flex-col lg:flex-row flex flex-col gap-[2%]">
                         <article>
-                            <Box w='100%' h='150px'>
-                                <img src={unila} alt="unila" style={{objectFit:"cover"}}/>
+                            <Box w='100%'>
+                                <img src={unila} alt="unila" style={{"object-fit": "cover"}}/>
                             </Box>
                             <h2 class="font-bold text-[13px] sm:text-xl">Universitas Lampung</h2>
                             <p class="text-[10px] sm:text-[18px] sm:font-medium pb-2">(2020 - Sekarang)</p>
-                        </article>
+                        </article><br />
                         <article>
-                            <Box w='100%' h='150px'>
+                            <Box w='100%'>
                                 <Image src={sma} alt="sma" style={{"object-fit": "cover"}}/>
                             </Box>
                             <h2 class="font-bold text-[13px] sm:text-xl">SMA YP UNILA</h2>
                             <p class="text-[10px] sm:text-[18px] sm:font-medium pb-2">(2017 - 2020)</p>
-                        </article>
+                        </article><br />
                         <article>
-                            <Box w='100%' h='150px'>
+                            <Box w='100%'>
                                 <Image src={smp} alt="unila" className="gambar"/>
                             </Box>
                             <h2 class="font-bold text-[13px] sm:text-xl">SMPN 02 Penawartama</h2>
                             <p class="text-[10px] sm:text-[18px] sm:font-medium pb-2">(2014 - 2017)</p>
-                        </article>
+                        </article><br />
                         <article>
-                            <Box w='100%' h='150px'>
+                            <Box w='100%'>
                                 <Image src={sd} alt="unila" className="gambar"/>
                             </Box>
                             <h2 class="font-bold text-[13px] sm:text-xl">SDN 01 Bogatama</h2>
