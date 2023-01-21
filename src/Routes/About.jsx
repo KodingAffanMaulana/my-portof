a6ddd0111c7df8c7499e206309d511d0e6113c84
KodingAffanMaulana <affan.maulana2016@students.unila.ac.id>
2023-01-21 23:23:00 +0700

    revisi 21 Januari 2023 (2)

--- src/Routes/About.jsx
+++ src/Routes/About.jsx
@@ -1,4 +1,4 @@
-import { Center } from "@chakra-ui/react";
+import { Center, color } from "@chakra-ui/react";
 
 const About = () => {
     return (
@@ -8,14 +8,14 @@ const About = () => {
                     <section id="about" class="p-8 lg:p-16 bg-white rounded-[2rem]">
                         <h1 class="font-bold text-[16px] sm:text-2xl text-lime-1000 pb-1 sm:pb-3">About Me</h1><br />
                         <Center>
-                            <div class="sm:flex-col md:flex-col lg:flex-row flex-col gap-[10%]">
+                            <div className="about" class="sm:flex-col md:flex-col lg:flex-row flex-col gap-[10%]">
                                 <article>
                                     <p class="text-yellow-800 text-[10px] sm:text-[18px] sm:font-medium pb-4">Hola, saya Affan Maulana seorang yang memiliki cita-cita untuk memajukan IT di daerah saya yaitu Bogatama. Saya
                                         berasal dari Lampung dan saat ini bertempat tinggal di Bandarlampung. Saya sekarang sedang menempuh pendidikan S1 Teknik Informatika pada salah satu Univeristas Impian saya sejak kecil
                                         yaitu Universitas Lampung. Saya saat ini sedang berkecimpung dibidang Frontend Web Developer.</p>
                                     {/* <Image src={rg} alt="sma" width="500px" /> */}
                                     <h2 class="font-bold text-yellow-800 text-[10px] sm:text-[18px] sm:font-medium">Kemampuan yang dimiliki</h2>
-                                    <ul class="list-disc list-inside text-yellow-700" >
+                                    <ul class="list-disc list-inside text-yellow-700 text-[10px] sm:text-[18px] sm:font-medium">
                                         <li>HTML</li>
                                         <li>CSS</li>
                                         <li>Javascript</li>
