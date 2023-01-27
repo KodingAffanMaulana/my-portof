65c30362bfba7fa5ee8c64b9f039a96cd66d7297
KodingAffanMaulana <affan.maulana2016@students.unila.ac.id>
2023-01-28 01:45:46 +0700

    icon about

--- src/Routes/About.jsx
+++ src/Routes/About.jsx
@@ -1,42 +1,70 @@
-import { Center } from "@chakra-ui/react";
+import { Center, List, ListItem, ListIcon } from '@chakra-ui/react';
 
-const About = () => {
+import { AiFillHtml5 } from "react-icons/ai";
+import { DiCss3Full } from "react-icons/di";
+import { DiJavascript1 } from "react-icons/di";
+import { DiReact } from "react-icons/di";
+import { BsCloudDownload } from "react-icons/bs";
+
+function About() {
     return (
         <>
             <main className="main bg-gradient-to-tr from-[gray-900] to-[#00ffcc] p-4 sm:p-16">
                 <div className="drop-shadow-2xl">
                     <section id="about" class="p-8 lg:p-16 bg-white rounded-[2rem]">
-                        <h1 class="font-bold text-[16px] sm:text-2xl text-lime-1000 pb-1 sm:pb-3">About Me</h1><br />
+                        <h1 class="font-bold text-[16px] sm:text-2xl text-lime-1000">About Me</h1><br />
                         <Center>
-                            <div className="about" class="sm:flex-col md:flex-col lg:flex-row flex-col gap-[10%]">
+                            <div>
                                 <article>
-                                    <p class="text-yellow-800 text-[10px] sm:text-[18px] sm:font-medium pb-4">Hola, saya Affan Maulana seorang yang memiliki cita-cita untuk memajukan IT di daerah saya yaitu Bogatama. Saya
-                                        berasal dari Lampung dan saat ini bertempat tinggal di Bandarlampung. Saya sekarang sedang menempuh pendidikan S1 Teknik Informatika pada salah satu Univeristas Impian saya sejak kecil
+                                    <p class="text-yellow-800 text-[10px] sm:text-[18px] sm:font-medium pb-4 text-justify">Hola, saya Affan Maulana seorang yang memiliki cita-cita untuk memajukan IT di daerah saya yaitu Bogatama. Saya
+                                        berasal dari Lampung dan saat ini bertempat tinggal di Bandarlampung. Saya sekarang sedang menempuh pendidikan S1 Teknik Informatika pada salah satu Universitas impian saya sejak kecil
                                         yaitu Universitas Lampung. Saya saat ini sedang berkecimpung dibidang Frontend Web Developer.</p>
                                     {/* <Image src={rg} alt="sma" width="500px" /> */}
-                                    <h2 class="font-bold text-yellow-800 text-[10px] sm:text-[18px] sm:font-medium">Skil dan Kemampuan</h2>
-                                    <ul class="list-disc list-inside text-yellow-700 text-[10px] sm:text-[18px] sm:font-medium">
-                                        <li>HTML</li>
-                                        <li>CSS</li>
-                                        <li>Javascript</li>
-                                        <li>ReactJS</li>
-                                        <li>NodeJS</li>
-                                        <li>Web API, RESTful API</li>
-                                        <li>Software Engineering Git/Github/Hosting</li>
-                                        <li>DMBS MYSQL</li>
-                                        <li>Deployment Github Pages/Netlify/Hosting</li>
-                                        <li>Editing Canva</li>
-                                        <li>Microsoft Office</li>
-                                        <li>Editing Video Kinemaster</li>
-                                    </ul>
+                                    <h1 class="font-bold text-green-900 text-[12px] sm:text-[18px]">Skills</h1>
+                                    {/* <ul class="list-disc list-inside text-yellow-700 text-[10px] sm:text-[18px] sm:font-medium">
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>Javascript</li>
+                <li>ReactJS</li>
+                <li>NodeJS</li>
+                <li>Web API, RESTful API</li>
+                <li>Software Engineering Git/Github/Hosting</li>
+                <li>DMBS MYSQL</li>
+                <li>Deployment Github Pages/Netlify/Hosting</li>
+                <li>Editing Canva</li>
+                <li>Microsoft Office</li>
+                <li>Editing Video Kinemaster</li>
+            </ul> */}
+                                    <List spacing={3} class="text-[10px] sm:text-[18px]">
+                                        <ListItem>
+                                            <ListIcon as={AiFillHtml5} color='red.500' />
+                                            HTML (Hyper Text Markup Language)
+                                        </ListItem>
+                                        <ListItem>
+                                            <ListIcon as={DiCss3Full} color='blue.500' />
+                                            CSS (Cascading Style Sheet)
+                                        </ListItem>
+                                        <ListItem>
+                                            <ListIcon as={DiJavascript1} color='yellow.500' />
+                                            Javascript
+                                        </ListItem>
+                                        <ListItem>
+                                            <ListIcon as={DiReact} color='blue.700' />
+                                            ReactJS
+                                        </ListItem>
+                                        <ListItem>
+                                            <ListIcon as={BsCloudDownload} color='blue.200' />
+                                            Web API & RESTful API
+                                        </ListItem>
+                                    </List>
                                 </article>
                             </div>
                         </Center>
                     </section>
                 </div>
-            </main >
+            </main>
         </>
-    )
+    );
 }
 
 export default About;
--- src/Routes/Experience.jsx
+++ src/Routes/Experience.jsx
@@ -10,8 +10,8 @@ const Experience = () => {
                 <div className="drop-shadow-2xl">
                     <section id="experience" class="p-8 lg:p-16 bg-white">
                         <h1 class="font-bold text-[16px] sm:text-2xl text-lime-1000 pb-1 sm:pb-3">Experience</h1><br />
-                        <div class="sm:flex-col md:flex-col lg:flex-row flex flex-col gap-[10%]">
-                            <article>
+                        <div class="sm:flex-col md:flex-col lg:flex-row flex justify-center flex-col gap-[10%]">
+                            <article class="pb-4 sm:pb-[0]">
                                 <h2 class="font-bold text-[13px] sm:text-xl">Studi Independen Kampus Merdeka Batch 3</h2>
                                 <p class="text-[10px] sm:text-[18px] sm:font-medium pb-2">Frontend Engineering Ruangguru CAMP</p>
                                 <Image src={rg} alt="sma" width="500px" />
